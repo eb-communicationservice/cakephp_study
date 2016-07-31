2d1e38b41a7834eef5da55c77116c3f4ec41f9fa
横谷美遥 <yokotani.miharu@ditgroup.jp>
2016-07-31 14:44:36 +0900

    【新規】課題17：選択法

--- src/Controller/AlgorithmController.php
+++ src/Controller/AlgorithmController.php
@@ -616,4 +616,41 @@ class AlgorithmController extends AppController
 			$this->set('resultSortArray', $resultSortArray);
 		}
 	}
+	
+	/*
+	* 17.選択法
+	*/
+	public function choiceSort(){
+		
+		// フォームから値を取得
+		$inputArray = array('str' => $this->request->data('str'));
+		
+		// 入力値から、カンマ区切りで配列に変換
+		$sortArray = split(",", $inputArray['str']);
+		
+		// 空判定実行
+		// 入力値に空が含まれている場合
+		if (!Validate::isEmpty($inputArray)) {
+			
+			// viewにエラーメッセージを送信
+			$this->set('errorMsg', "値を入力して下さい");
+		
+		// 数字判定実行
+		// 入力値に数字ではない値が含まれている場合
+		} elseif (!Validate::isNumeric($sortArray)) {
+			
+			// viewにエラーメッセージを送信
+			$this->set('errorMsg', "数字ではない文字が含まれています");
+		
+		// 入力値数字の場合
+		} else {
+			
+			// バブルソート実行
+			$resultSortArray = AlgorithmUtils::execChoiceSort($sortArray);
+			
+			// viewに結果を送信
+			$this->set('errorMsg', "");
+			$this->set('resultSortArray', $resultSortArray);
+		}
+	}
 }
